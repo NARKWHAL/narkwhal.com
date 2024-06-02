781872c6fd23e91bd19d65d3ec7b7e703c284373
NARKWHAL <n4rkwh4l@proton.me>
2024-06-02 12:56:11 -0400

    edit

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🦄👮‍♂🐳 Narkwhal",
+    pageTitle: "🦄Narkwhal🐳",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
